626b459a7e360e4e4a982a3f6c1b8ee016f874ce
Sama2424 <sama.ehab@msa.edu.eg>
2024-05-23 14:56:33 +0300

    changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('HELLO Cloud world'));
+app.get('/', (req, res) => res.send('HELLO Cloud world 2:55'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
